c9427c74b29da8a9ffcd012b35879f73d194c314
anumoluph13 <anumoluph13@gmail.com>
2024-02-05 18:20:48 +0000

    code

---
+++ src/main/java/com/javatechie/githubcicdactions/GithubCicdActionsApplication.java
@@ -0,0 +1,25 @@
+package com.javatechie.githubcicdactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+	@GetMapping("\welcom")
+	public String welcome(){
+		return "Welcome for myFirst Springboot App!!!";
+	}
+	public SomeData getMethodName(@RequestParam String param) {
+		return new SomeData();
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+
+}
